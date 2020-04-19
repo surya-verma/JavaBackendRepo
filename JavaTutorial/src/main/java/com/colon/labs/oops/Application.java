12e5f603aa27d87d5ef2f95263382a8947b277b7
mymacbook_raksbini <mymacbook_raksbini@10.0.0.142>
2020-04-19 14:44:10 -0400

    Test Commit

--- JavaTutorial/src/main/java/com/colon/labs/oops/Application.java
+++ JavaTutorial/src/main/java/com/colon/labs/oops/Application.java
@@ -1,29 +1,16 @@
 package com.colon.labs.oops;
 
-import java.util.Scanner;
-
 public class Application extends Car {
 
-	public  void start(String model, int year) {
-		
-	    
-		
-		Car myCar=new Car();
-
-		
+	public  void start(String model, int year) {	
+	    	
+		Car myCar=new Car();		
 		String engine =myCar.engine(model);
 		String brand = myCar.brand(model);
-		StringBuilder brandY= myCar.brand(model, year);
-		
+		StringBuilder brandYear= myCar.brand(model, year);		
 		myCar.type(model);
 		System.out.println("Your Car's engine is:" + engine);
-		System.out.println("Your Car's brand is:" +brand);
-		
-		System.out.println("Your Car's brand is:" +brandY);
-		
-		
+		System.out.println("Your Car's brand is:" +brand);		
+		System.out.println("Your Car's brand is:" +brandYear);			
 	}
-	
-	
-
 }
